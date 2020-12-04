d61281a799160138e7183b9a4f905cc3a21170d6
Michael Marshall <michaelmarshall220@gmail.com>
2020-12-03 22:34:47 -0600

    bug fix

--- cord-persistence/src/main/kotlin/com/seedcompany/cord/model/EmailToken.kt
+++ cord-persistence/src/main/kotlin/com/seedcompany/cord/model/EmailToken.kt
@@ -1,12 +1,16 @@
 package com.seedcompany.cord.model
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import org.springframework.data.neo4j.core.schema.Id
 import org.springframework.data.neo4j.core.schema.Node
 import org.springframework.data.neo4j.core.schema.Relationship
 import java.time.ZonedDateTime
 
 @Node
 class EmailToken (
-        override var id: String,
+        @Id
+        var id: String,
         var email: String,
-): BaseNode()
+) {
+    val createdAt: ZonedDateTime = ZonedDateTime.now()
+}
--- cord-persistence/src/main/kotlin/com/seedcompany/cord/model/Token.kt
+++ cord-persistence/src/main/kotlin/com/seedcompany/cord/model/Token.kt
@@ -1,14 +1,18 @@
 package com.seedcompany.cord.model
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import org.springframework.data.neo4j.core.schema.Id
 import org.springframework.data.neo4j.core.schema.Node
 import org.springframework.data.neo4j.core.schema.Relationship
 import java.time.ZonedDateTime
 
 @Node
 class Token (
-        override var id: String,
+        @Id
+        var id: String,
         @Relationship(type = "token", direction = Relationship.Direction.INCOMING)
         @JsonIgnore
         var user: User? = null,
-): BaseNode()
+) {
+        val createdAt: ZonedDateTime = ZonedDateTime.now()
+}
